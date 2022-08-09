5e4450059106bf672194d507d18f1fe546e5f094
Kübra Beceren <101129228+KbrBcrn@users.noreply.github.com>
2022-08-09 20:39:46 +0200

    Add files via upload

---
+++ ft_printf.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kbeceren <kbeceren@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/08 19:51:57 by kbeceren          #+#    #+#             */
+/*   Updated: 2022/08/09 20:39:37 by kbeceren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_printf(const char *format, ...)
+{
+	// Declaring pointer to the
+   		// argument list
+	va_list	args;
+	int		i;
+	int		count;
+
+	i = 0;
+	count = 0;
+	// Initializing argument to the
+    	// list pointer
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			// Accessing current variable
+        		// and pointing to next one
+			count += ft_formats(args, format[i + 1]);
+			i++;
+		}
+		else
+			count += ft_putchar(format[i]);
+		i++;
+	}
+	// Ending argument list traversal
+	va_end(args);
+	return (count);
+}
